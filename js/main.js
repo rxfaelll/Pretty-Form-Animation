ee257c3ac03a0dd2823a842e0bf4226466fde8e0
rxfaelll <rafa.081299@gmail.com>
2019-09-15 01:27:13 -0300

    Creating welcome message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const inputForm = document.querySelectorAll(".input-block input");
 const submitBtn = document.querySelector(".submit-btn");
 const form = document.querySelector("form");
+const welcomeMsg = document.querySelector("#welcomeMsg");
 
 submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
@@ -11,10 +12,24 @@ function validateForm(){
     inputForm.forEach( inputForm => {
         if(inputForm.value == ""){
             form.classList.add("form-refuse");
+        }else{
+            form.classList.add("form-accept");
+            welcomeMsgShow();
         }
-    });    
+    });
 }
 
 form.addEventListener("animationend", () =>{
     form.classList.remove("form-refuse");
 })
+
+function welcomeMsgShow(){
+    let username = inputForm[0].value;
+
+    welcomeMsg.classList.add("active-msg");
+    welcomeMsg.innerHTML = `Welcome, ${username}, glad to see you back!`;
+
+    setTimeout(() => {
+        form.style.display = "none";
+    }, 500);
+}
